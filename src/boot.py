ee36e6539a134bce404786674d0546673607ddbc
Alexander Kläser <github@klaeser.me>
2024-11-12 18:34:11 +0100

    Initial version of the code

---
+++ src/boot.py
@@ -0,0 +1,117 @@
+import network
+import time
+import tools
+import machine
+import binascii
+
+# Netzwerkeinstellungen für LAN mit DHCP konfigurieren (siehe nächster Abschnitt)
+def setup_lan():
+    network.hostname("mio-cash-register")
+    lan = network.LAN(mdc = machine.Pin(23), mdio = machine.Pin(18), power = machine.Pin(12), phy_type = network.PHY_LAN8720, phy_addr = 0)
+    lan.active(True)
+    #lan.ifconfig('dhcp')
+    print("Warte auf Netzwerkverbindung...")
+    for i in range(10):
+        if lan.isconnected():
+            break
+        time.sleep(1)
+    if lan.isconnected():
+        print("Verbunden! IP-Adresse:", lan.ifconfig()[0])
+    else:
+        print("Keine Netzverbindung!")
+
+# Rufe setup_lan auf, um das LAN mit DHCP zu aktivieren
+setup_lan()
+
+from microdot import Microdot, Response
+from microdot.utemplate import Template
+
+app = Microdot()
+Response.default_content_type = 'text/html'
+USERNAME, PASSWORD = tools.load_credentials()
+
+# Hilfsfunktion für Basic Authentication
+def check_basic_auth(request):
+    auth = request.headers.get('Authorization')
+    if not auth:
+        return False
+
+    # Erwartet "Basic <base64-encoded username:password>"
+    try:
+        auth_type, credentials = auth.split(" ")
+        if auth_type != "Basic":
+            return False
+
+        # Base64-Dekodierung der Anmeldeinformationen
+        decoded_credentials = binascii.a2b_base64(credentials).decode("utf-8")
+        username, password = decoded_credentials.split(":")
+
+        return username == USERNAME and password == PASSWORD
+    except Exception:
+        return False
+    
+
+# Authentifizierungs-Wrapper für geschützte Routen
+def requires_auth(handler):
+    async def wrapper(request, *args, **kwargs):
+        if not check_basic_auth(request):
+            return Response(status_code=401, headers={
+                'WWW-Authenticate': 'Basic realm="Authentication Required"'
+            }, body="Unauthorized")
+        return await handler(request, *args, **kwargs)
+    return wrapper
+
+
+@app.route('/test')
+@requires_auth
+async def index(request):
+    print('/test GET')
+    return Template("dummy.html").render(name='Alex')
+
+
+@app.route('/')
+@requires_auth
+async def index(request):
+    print('/ GET')
+    tags = tools.load_authorized_uids()
+    print(tags)
+    return Template("main.html").render(tags=tags)
+
+
+@app.put('/tags')
+@requires_auth
+async def add_tag(request):
+    print('/tags PUT')
+    new_tag = request.json
+    print('  {}'.format(new_tag))
+    
+    if 'username' in new_tag and 'timestamp' in new_tag:
+        uid = await tools.read_uid()
+        if uid is None:
+            return {'success': False}, 400
+        else:
+            tools.add_uid(uid, new_tag['username'], new_tag['timestamp'])
+            return {'success': True}, 200
+    else:
+        return {'success': False}, 400
+    
+
+@app.delete('/tags')
+@requires_auth
+async def delete_tag(request):
+    print('/tags DELETE')
+    params = request.args
+    print('  {}'.format(params))
+    
+    if 'uid' in params:
+        tools.remove_uid(params['uid'])
+        return {'success': True}, 200
+    else:
+        return {'success': False}, 400
+
+
+print("Starting RFID reading...")
+tools.start_rfid_reading()
+
+print("Starting web server...")
+app.run(port=80)
---
+++ src/tools.py
@@ -0,0 +1,113 @@
+import ujson
+import os
+from machine import Pin, SoftSPI
+import time
+from mfrc522 import MFRC522
+import _thread
+import random
+import asyncio
+
+
+def load_credentials():
+    with open("credentials.txt", "r") as f:
+        username = f.readline().strip()
+        password = f.readline().strip()
+    return username, password
+
+USERNAME, PASSWORD = load_credentials()
+
+
+def load_authorized_uids():
+    try:
+        with open("/authorized_uids.json", "r") as f:
+            return ujson.load(f)
+    except:
+        return []
+    
+
+_tags = load_authorized_uids()
+
+def save_authorized_uids(tags):
+    global _tags
+    with open("/authorized_uids.json", "w") as f:
+        ujson.dump(tags, f)
+    _tags = tags
+
+
+def add_uid(uid, username, timestamp):
+    global _tags
+    if not is_uid_registered(uid):
+        _tags.append(dict(uid=uid, username=username, timestamp=timestamp))
+        save_authorized_uids(_tags)
+
+
+def remove_uid(uid):
+    global _tags
+    tags = [i for i in _tags if i['uid'] != uid]
+    if len(tags) != len(_tags):
+        save_authorized_uids(tags)
+
+
+def is_uid_registered(uid):
+    global _tags
+    uids = set([i['uid'] for i in _tags])
+    return uid in uids
+
+
+sck = Pin(14, Pin.OUT)
+mosi = Pin(15, Pin.OUT)
+miso = Pin(35)
+spi = SoftSPI(baudrate=1000000, polarity=0, phase=0, sck=sck, mosi=mosi, miso=miso)
+sda = Pin(2, Pin.OUT)
+reader = MFRC522(spi, sda)
+
+
+async def open_cash_register():
+    relay = Pin(13, Pin.OUT)
+    relay.value(0)
+    await asyncio.sleep_ms(300)
+    relay.value(1)
+    
+
+def start_rfid_reading():
+    asyncio.create_task(_rfid_reading())
+
+
+_last_uid = None
+_n_status_err = 0
+async def _rfid_reading():
+    global _last_uid, _n_status_err
+    while True:
+        (status, tag_type) = reader.request(reader.CARD_REQIDL)
+        if status == reader.OK:
+            (status, raw_uid) = reader.anticoll()
+            if status == reader.OK:
+                uid = ''.join('{:02X}'.format(x) for x in raw_uid)
+                print('_rfid_reading: Card Detected: %s' % uid)
+                print('')
+                if _last_uid is None and is_uid_registered(uid):
+                    await open_cash_register()
+                _last_uid = uid
+                _n_status_err = 0
+        else:
+            _n_status_err += 1
+            if _n_status_err >= 2:
+                _last_uid = None
+
+        await asyncio.sleep_ms(250)
+
+
+async def read_uid():
+    global _last_uid
+    
+    # try a few times to read the UID
+    for i in range(20):
+        if _last_uid is not None:
+            return _last_uid
+        # allow for some randomness here, to avoid race conditions with RFID reading thread
+        await asyncio.sleep_ms(random.randint(200, 300))
+    
+    # no card read until now
+    return None
+
+
